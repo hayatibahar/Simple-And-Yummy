14ae8b8a25453457db34339d0ec7536105e1648e
PC <hayati.458@hotmail.com>
2024-01-27 17:29:52 +0300

    update common

---
+++ app/src/main/java/com/hayatibahar/simpleandyummy/core/common/DataStoreConstants.kt
@@ -0,0 +1,8 @@
+package com.hayatibahar.simpleandyummy.core.common
+
+object DataStoreConstants {
+
+    const val RECIPE_DATA_STORE = "recipe_data_store"
+    const val LAST_REQUEST_TIME = "last_request_time"
+    const val IS_DARK_MODE = "is_dark_mode"
+}
